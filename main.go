612bc29fe113b5b1dcb490c9ad2f0db4e32e045c
KNS121 <komaroff.ns@gmail.com>
2026-06-30 18:58:48 +0300

    read dir or files and input in array

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: TriageExporter.exe <file|folder>")
-		fmt.Println("Example: TriageExporter.exe Process.jsonl")
+		fmt.Println("Usage: TriageExporter <file|folder>")
 		return
 	}
 
@@ -19,11 +20,33 @@ func main() {
 		panic(err)
 	}
 
+	var files []string
+
 	if info.IsDir() {
 		fmt.Println("Input is folder:", path)
+
+		filepath.Walk(path, func(p string, f os.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			if !f.IsDir() && strings.HasSuffix(f.Name(), ".jsonl") {
+				files = append(files, p)
+			}
+			return nil
+		})
 	} else {
 		fmt.Println("Input is file:", path)
+		files = append(files, path)
 	}
 
-	fmt.Println("READY")
+	fmt.Printf("\nFound %d files:\n", len(files))
+	for i, f := range files {
+		info, err := os.Stat(f)
+		if err == nil {
+			fmt.Printf(" %d. %s (size: %d bytes)\n", i+1, f, info.Size())
+		}
+	}
+
+	fmt.Println("\nREADY FOR NEXT STEP")
 }
